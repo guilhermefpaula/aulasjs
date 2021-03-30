1faa8edaba2e18b8972f1d8baf6afeab5ccc87b4
guilhermefpaula <guilhermefpaula@hotmail.com>
2021-03-30 16:37:27 -0300

    atualizei variaveis em index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let minha_altura = 1.75
 let minha_idade = 27
-const Pi = 3.14169265
-const Parsec = 3.26156 
-const lightyear = 9460730472580.8
-let crush = true
+const pi = 3.14169265
+const parsec_em_ano_luz = 3.26156 
+const ano_luz_em_km = 9460730472580.8
+let estou_acordado = true
 let está_chovendo = false
 let aguei_as_plantas = false
 let lavei_a_louça = true
@@ -16,10 +16,10 @@ let melhor_professor = "Jeffinho gostoso"
 
 console.log(typeof minha_altura)
 console.log(typeof minha_idade)
-console.log(typeof Pi)
-console.log(typeof Parsec)
-console.log(typeof lightyear)
-console.log(typeof crush)
+console.log(typeof pi)
+console.log(typeof parsec_em_ano_luz)
+console.log(typeof ano_luz_em_km)
+console.log(typeof estou_acordado)
 console.log(typeof está_chovendo)
 console.log(typeof aguei_as_plantas)
 console.log(typeof lavei_a_louça)
@@ -29,4 +29,3 @@ console.log(typeof clima)
 console.log(typeof musica_favorita)
 console.log(typeof dia_da_semana)
 console.log(typeof melhor_professor)
-
