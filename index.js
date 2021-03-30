8a27285009034b6bee0544b893bdd3c0bfb842f6
guilhermefpaula <guilhermefpaula@hotmail.com>
2021-03-30 15:17:40 -0300

    acrescentei a resolução da lição referente a aula 1

---
+++ index.js
@@ -0,0 +1,32 @@
+let minha_altura = 1.75
+let minha_idade = 27
+const Pi = 3.14169265
+const Parsec = 3.26156 
+const lightyear = 9460730472580.8
+let crush = true
+let está_chovendo = false
+let aguei_as_plantas = false
+let lavei_a_louça = true
+let varri_a_casa = true
+let meu_nome = "Guilherme Falconi"
+let clima = "ameno"
+let musica_favorita = "Paschendale"
+let dia_da_semana = "Terça-feira"
+let melhor_professor = "Jeffinho gostoso"
+
+console.log(typeof minha_altura)
+console.log(typeof minha_idade)
+console.log(typeof Pi)
+console.log(typeof Parsec)
+console.log(typeof lightyear)
+console.log(typeof crush)
+console.log(typeof está_chovendo)
+console.log(typeof aguei_as_plantas)
+console.log(typeof lavei_a_louça)
+console.log(typeof varri_a_casa)
+console.log(typeof meu_nome)
+console.log(typeof clima)
+console.log(typeof musica_favorita)
+console.log(typeof dia_da_semana)
+console.log(typeof melhor_professor)
+
